d7168cf0705319a316a15fa67346f644ad4cc95c
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-30 07:41:35 -0500

    Product is not valid without a unique title

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
 
+	fixtures :products
+
 	test "product attributes must not be empty" do
 		product = Product.new
 		assert product.invalid?
@@ -45,6 +47,15 @@ class ProductTest < ActiveSupport::TestCase
 		bad.each do |name|
 			assert new_product(name).invalid?, "#{name} should not be valid"
 		end
+	end
+
+	test "product is not valid without a unique title" do
+		product = Product.new(title: 			 products(:ruby).title,
+													description: "Lorem ipsum color sit amet",
+													image_url: 	 "example.jpg",
+													price:    	 1)
+		assert product.invalid?
+		assert_equal ["has already been taken"], product.errors[:title]
 	end
 
 end
