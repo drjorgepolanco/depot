521b44c24365403ff17e9c7d38f9ed9e1b318506
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-31 01:37:10 -0500

    Add comment about the reason we created the set_cart method there inside a Module

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -4,9 +4,16 @@ module CurrentCart
 	private
 
 		def set_cart
+			# Gets the :cart_id from the session object and
+			# attemps to find a cart corresponding to this ID.
 			@cart = Cart.find(session[:cart_id])
 		rescue ActiveRecord::RecordNotFound
+			# If no carts where found, it creates a new one.
 			@cart = Cart.create
 			session[:cart_id] = @cart.id
 		end
 end
+
+# We place the set_cart() method in a CurrentCart module and mark it as private. 
+# This allows us to share common code between controllers and prevents Rails 
+# from making it available as an action on the controller.
