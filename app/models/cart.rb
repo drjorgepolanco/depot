63e14c4a3cdf2ea94c6c94b72ae796f553a27135
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-01 04:42:43 -0500

    Add explanatory comments to add_product

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,8 +4,10 @@ class Cart < ActiveRecord::Base
 	def add_product(product_id)
 		current_item = line_items.find_by(product_id: product_id)
 		if current_item
+			# Updates the item quantity if it already exists in the cart
 			current_item.quantity += 1
 		else
+			# Or create a new one if not
 			current_item = line_items.build(product_id: product_id)
 		end
 		current_item
