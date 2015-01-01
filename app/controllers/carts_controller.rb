d224b88a4f18a78c5a26aa06c6c8d98a777e17e1
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-01 05:19:39 -0500

    Create method invalid cart to display error message when a wrong cart is submitted through the browser

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,34 +1,28 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  # Intercepts the exception raised by Cart.find()
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
-  # GET /carts
-  # GET /carts.json
   def index
     @carts = Cart.all
   end
 
-  # GET /carts/1
-  # GET /carts/1.json
   def show
   end
 
-  # GET /carts/new
   def new
     @cart = Cart.new
   end
 
-  # GET /carts/1/edit
   def edit
   end
 
-  # POST /carts
-  # POST /carts.json
   def create
     @cart = Cart.new(cart_params)
-
     respond_to do |format|
       if @cart.save
-        format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
+        format.html { redirect_to @cart, 
+                      notice: 'Cart was successfully created.' }
         format.json { render :show, status: :created, location: @cart }
       else
         format.html { render :new }
@@ -37,12 +31,11 @@ class CartsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /carts/1
-  # PATCH/PUT /carts/1.json
   def update
     respond_to do |format|
       if @cart.update(cart_params)
-        format.html { redirect_to @cart, notice: 'Cart was successfully updated.' }
+        format.html { redirect_to @cart, 
+                      notice: 'Cart was successfully updated.' }
         format.json { render :show, status: :ok, location: @cart }
       else
         format.html { render :edit }
@@ -51,24 +44,30 @@ class CartsController < ApplicationController
     end
   end
 
-  # DELETE /carts/1
-  # DELETE /carts/1.json
   def destroy
     @cart.destroy
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to carts_url, 
+                    notice: 'Cart was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_cart
       @cart = Cart.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def cart_params
       params[:cart]
+    end
+
+    
+    def invalid_cart
+      # Record a message at the error logging level.
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      # Redirect to the catalog display
+      redirect_to store_url, notice: 'Invalid cart'
     end
 end
