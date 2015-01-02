dcc60051d2b304bb432e0f93e4df1c5ebd4fdb9e
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-02 01:41:06 -0500

    Create who_bought route as a member of products

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :orders
-
   resources :line_items
-
   resources :carts
 
   root 'store#index', as: 'store'
   get 'store/index'
-  resources :products
+  resources :products do
+  	get :who_bought, on: :member
+  end
 end
