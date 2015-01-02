3f1c9ef7f02dca3d04d7e4213a23f945c4159b9a
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-02 08:54:51 -0500

    Modify new user name

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -18,7 +18,7 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: { name: "Julito", password: 'secret', 
+      post :create, user: { name: "Triculi", password: 'secret', 
                             password_confirmation: 'secret' }
     end
 
