c9870282b00c4b9a76346b71fdfc93bf5dd9f43d
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-02 09:23:44 -0500

    Update routes to tell rails which actions are to respond to get, post and delete in sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
-  get 'admin/index'
-
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
+  get 'admin' => 'admin/index'
+  controller :sessions do
+    get    'login'  => :new
+    post   'login'  => :create
+    delete 'logout' => :destroy
+  end
 
   resources :users
   resources :orders
   resources :line_items
   resources :carts
 
-  root 'store#index', as: 'store'
   get 'store/index'
   resources :products do
   	get :who_bought, on: :member
   end
+
+  root 'store#index', as: 'store'
 end
