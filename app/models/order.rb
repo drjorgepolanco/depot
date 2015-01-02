142f2361eaa1c39f6cce53ad844350cf327ef598
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-02 00:03:06 -0500

    Create method add_line_items_from_cart

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,13 @@ class Order < ActiveRecord::Base
 	PAYMENT_TYPES = [ "Check", "Credit Card", "Purchase Order" ]
 	validates :name, :address, :email, presence: true
 	validates :pay_type, inclusion: PAYMENT_TYPES
+
+	def add_line_items_from_cart(cart)
+		cart.line_items.each do |item|
+			# prevent the item from going poof when we destroy the cart
+			item.cart_id = nil
+			# add the item itself to the line_items collection for the order
+			line_items << item
+		end
+	end
 end
