53a548233557cef8ba293d85c74dadf83188707e
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-03 00:57:32 -0500

    Remove scope locale'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'admin' => 'admin#index'
-
   controller :sessions do
     get    'login'  => :new
     post   'login'  => :create
@@ -8,15 +7,14 @@ Rails.application.routes.draw do
   end
 
   resources :users
+  resources :orders
+  resources :line_items
+  resources :carts
 
+  get 'store/index'
   resources :products do
   	get :who_bought, on: :member
   end
 
-  scope '(:locale)' do
-    resources :orders
-    resources :line_items
-    resources :carts
-    root 'store#index', as: 'store', via: :all
-  end
+  root 'store#index', as: 'store'
 end
