8f526d4c1a0be8cfaf62d3ead8f1afaacec1b02e
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-01-03 15:06:18 -0500

    Fix merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'admin' => 'admin#index'
+
   controller :sessions do
     get    'login'  => :new
     post   'login'  => :create
@@ -7,14 +8,15 @@ Rails.application.routes.draw do
   end
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
 
-  get 'store/index'
   resources :products do
-  	get :who_bought, on: :member
+    get :who_bought, on: :member
   end
 
-  root 'store#index', as: 'store'
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
 end
